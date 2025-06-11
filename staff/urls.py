5fa3195f2af7fb26d8dc3f99253c41782c359c8e
zaagaa <zaagaa.team@gmail.com>
2025-06-11 09:44:20 +0530

    fx

--- staff/urls.py
+++ staff/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
-from . import views
+
+from staff.views.attendance_summary import attendance_summary
+from staff.views.leave_booking import book_leave, get_leave_summary, delete_leave, calendar_leave_status
+from staff.views.profile import staff_profile
 
 urlpatterns = [
-    path('profile/', views.staff_profile, name='staff_profile'),
-path('leave/', views.book_leave, name='book_leave'),
-path('leave/summary/', views.get_leave_summary, name='leave_summary'),
-path('leave/delete/<uuid:leave_id>/', views.delete_leave, name='delete_leave'),
-path('leave/calendar-data/', views.calendar_leave_status, name='calendar_leave_status'),
+    path('profile/', staff_profile, name='staff_profile'),
+path('leave/', book_leave, name='book_leave'),
+path('leave/summary/', get_leave_summary, name='leave_summary'),
+path('leave/delete/<uuid:leave_id>/', delete_leave, name='delete_leave'),
+path('leave/calendar-data/', calendar_leave_status, name='calendar_leave_status'),
 
 
-path("my-attendance/", views.attendance_summary, name="staff_attendance_summary"),
+path("my-attendance/", attendance_summary, name="staff_attendance_summary"),
 
 
 
