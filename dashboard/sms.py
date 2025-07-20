173000aa7702af91b72ddb24db9a20bf9cd89acd
zaagaa <zaagaa.team@gmail.com>
2025-07-20 11:03:03 +0530

    updated@20/07/2025 11.03 AM

--- dashboard/sms.py
+++ dashboard/sms.py
@@ -3,7 +3,10 @@ import urllib.parse
 from datetime import datetime
 
 def send_otp_sms(name, mobile, point, otp):
-    content = f"Hello {name} ID:OTP: {otp}, You Account has been Updated Successfully. Your Current Point is {point:.2f}. OTP: {otp}. Thank you for Shopping @ Kurinji"
+    content = (
+        f"Hello {name} ID:OTP: {otp}, You Account has been Updated Successfully. "
+        f"Your Current Point is {point:.2f}. OTP: {otp}. Thank you for Shopping at Kurinji"
+    )
     encoded_msg = urllib.parse.quote(content)
     dtTimeNow = urllib.parse.quote(datetime.now().strftime('%m/%d/%Y %I:%M:%S %p'))
 
@@ -20,9 +23,16 @@ def send_otp_sms(name, mobile, point, otp):
         "&tempid=1607100000000118145"
     )
 
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+    }
+
+    print(url)
+
     try:
-        response = requests.get(url, timeout=10)
-        return response.status_code == 200
+        response = requests.get(url, headers=headers, timeout=25)
+        print("Response:", response.status_code, response.text)
+        return response.status_code == 200 and "Msg successfully sent" in response.text
     except Exception as e:
         print(f"❌ SMS Error: {e}")
         return False
